553298e66af96febe4925592cd8d1398b5e34007
yattinda <yattinda@gmail.com>
2021-01-10 22:00:12 +0900

    solve 36

--- 36-40.c
+++ 36-40.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int is_perfect(int n) {
+  int sum = 0, i;
+
+  for (i=1; i<n; i++) {
+    if (n % i == 0) {
+      sum += i;
+    }
+  }
+
+  if (sum == n) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int main(void) {
+  int p;
+  scanf("%i", &p);
+  printf("%i\n", is_perfect(p));
+  return 0;
+}

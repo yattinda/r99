0102b1d1c7219fb3da3d5e29e5dbef0e44daba49
yattinda <yattinda@gmail.com>
2021-01-14 10:33:25 +0900

    Done

--- 46-50.c
+++ 46-50.c
@@ -51,3 +51,46 @@ int f_to_i(float x) {
 // }
 
 //49
+//引数を10倍にし、(int)xの差を比較して四捨五入した
+float f_to_f1(float x){
+  x *= 10;
+  if (x - (int)x < 0.5) {
+    return (int)x * 0.1;
+  } else {
+   return ((int)x * 0.1) + 0.1;
+  }
+}
+
+int main(void) {
+  float p;
+  scanf("%f", &p);
+  printf("%f\n", f_to_f1(p));
+  return 0;
+}
+
+//50
+//aのb乗を返す関数を作り、49と同じ流れで四捨五入した
+float factorial(float a, float b){
+  int i;
+  float fact = 1;
+  for (i=0; i<b; i++){
+    fact *= a;
+  }
+  return fact;
+}
+
+float f_to_f(float x, int n){
+  x *= factorial(10, n);
+  if (x - (int)x < 0.5) {
+    return (int)x * factorial(0.1, n);
+  } else {
+   return ((int)x * factorial(0.1, n)) + factorial(0.1, n);
+  }
+}
+
+int main(void) {
+  float p, q;
+  scanf("%f %f", &p ,&q);
+  printf("%f\n", f_to_f(p, q));
+  return 0;
+}

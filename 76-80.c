ba0ed54b8a72a3cbda88ae5eab3cf8fc2f255be9
yattinda <yattinda@gmail.com>
2021-01-21 23:54:02 +0900

    do 78 79

--- 76-80.c
+++ 76-80.c
@@ -2,3 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+
+//78
+// 文字列の最初が空文字だったら1を返す
+int is_empty(char* s) {
+  if (s[0] == '\0') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int main(void) {
+  char s[10];
+  scanf("%s", s);
+  printf("%i\n", is_empty(s))
+
+  return 0;
+}
+
+//79
+int str_len(char* s) {
+  int total;
+  for(total=0; s[total]!='\0'; total++) {
+    total++;
+  }
+  return total;
+}
+
+int main(void) {
+  char s[50] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  printf("%s %i \n", s, str_len(s));
+  return 0;
+}

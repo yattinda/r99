5481c8284ce7e44a923689bea5e46035f8759a12
yattinda <yattinda@gmail.com>
2021-01-28 10:07:14 +0900

    Do 56

--- 56-60.c
+++ 56-60.c
@@ -2,3 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+
+//56
+//ファイルを読み込み、戦闘をfscanfで読み込んだものを返した
+int head(void){
+  FILE* fp;
+  int readnum = 0;
+
+  fp = fopen("numbers.txt", "r");
+  fscanf(fp, "%i", &readnum);
+  fclose(fp);
+
+  return readnum;
+}
+
+int main(void){
+  printf("%i\n", head());
+}
+
+//57

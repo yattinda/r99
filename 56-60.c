a97fb55e770d3a90eb71bb0c8478d8d53eb118c5
yattinda <yattinda@gmail.com>
2021-01-31 23:39:55 +0900

    Do 56 57

--- 56-60.c
+++ 56-60.c
@@ -21,3 +21,22 @@ int main(void){
 }
 
 //57
+// fgets()で改行ごとにNULLが返るまでcountを足していった
+int lines(void) {
+  FILE* fp;
+  int count;
+  char c_num[100];
+
+  fp = fopen("numbers.txt", "r");
+  for (count=0; fgets(c_num, 100, fp); ) {
+    count++;
+  }
+  fclose(fp);
+
+  return count;
+}
+
+int main(void) {
+  printf("%i\n", lines());
+  return 0;
+}

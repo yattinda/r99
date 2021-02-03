155e38cf9b16ccb704aa2add35e71712b3afe5a5
yattinda <yattinda@gmail.com>
2021-02-03 10:24:58 +0900

    Undone 95

--- 91-95.c
+++ 91-95.c
@@ -8,7 +8,7 @@
 int str_len(char* s) {
   int total;
   for(total=0; s[total]!='\0'; total++) {
-    total++;
+    ;
   }
   return total;
 }
@@ -56,44 +56,10 @@ int main(void){
 }
 
 //93
-char* int_to_str(int a) {
-  char* ans  =(char*)malloc(sizeof(char) * 1000);
-  int i, p_or_n  = 0;
-  if(a < 0){
-    p_or_n = -1;
-    a = -a;
-  }
-
-  for(i=0; a!=0; i++){
-    ans[i] = (a % 10) + 48;
-    a /= 10;
-  }
-  if(p_or_n == -1){
-    ans[i] = '-';
-  }
-  return ans;
-}
-
-int main(void){
-  int p = 114514;
-  char* q;
-  q = int_to_str(p);
-  printf("%s", q);
-  return 0;
-}
-
-int main(void){
-  int p = -114514;
-  char* q;
-  q = int_to_str(p);
-  printf("%s", q);
-  return 0;
-}
-//94
 int str_len(char* s) {
   int total;
   for(total=0; s[total]!='\0'; total++) {
-    total++;
+    ;
   }
   return total;
 }
@@ -110,6 +76,67 @@ char* str_reverse(char* s){
     return s;
 }
 
+/*mallocでメモリを確保し、はじめに正負で場合分けをする。その後１の位から1桁づつ
+'\0'である48を足していき配列に格納した。最後に負だった場合-をつけた
+最後に配列に入っている順序が逆なので94で整形した*/
+char* int_to_str(int a) {
+  char* ans  =(char*)malloc(sizeof(char) * 1000);
+  int i, p_or_n  = 0;
+  if(a < 0){
+    p_or_n = -1;
+    a = -a;
+  }
+
+  for(i=0; a!=0; i++){
+    ans[i] = (a % 10) + 48;
+    a /= 10;
+  }
+  if(p_or_n == -1){
+    ans[i] = '-';
+  }
+  ans = str_reverse(ans);
+  return ans;
+}
+
+int main(void){
+  int p = -114514;
+  char* q;
+  q = int_to_str(p);
+  printf("%s", q);
+  return 0;
+}
+
+int main(void){
+  int p = 114514;
+  char* q;
+  q = int_to_str(p);
+  printf("%s", q);
+  return 0;
+}
+
+
+//94
+int str_len(char* s) {
+  int total;
+  for(total=0; s[total]!='\0'; total++) {
+    ;
+  }
+  return total;
+}
+
+//外側から順に文字を入れ替えていく
+char* str_reverse(char* s){
+  int len_size = str_len(s);
+  int i;
+
+  for(i=0; i<len_size/2; i++){
+    char tmp = s[i];
+    s[i] = s[len_size - i - 1];
+    s[len_size - i - 1] = tmp;
+  }
+  return s;
+}
+
 int main(void){
   char p[100] = "abcdefg";
   printf("%s", str_reverse(p));
@@ -117,3 +144,21 @@ int main(void){
 }
 
 //95
+
+char* jstr_reverse(char* s){
+  int len_size = str_len(s);
+  int i;
+
+  for(i=0; i<len_size/2; i++){
+    char tmp = s[i];
+    s[i] = s[len_size - i - 1];
+    s[len_size - i - 1] = tmp;
+  }
+  return s;
+}
+
+int main(void){
+  char p[100] = "abcdefg";
+  printf("%s", str_reverse(p));
+  return 0;
+}
